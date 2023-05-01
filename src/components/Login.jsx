291e80e06a1a5a4275f8288def4e95b10f9ca58e
Sohag Rana <sohagrana921@gmail.com>
2023-05-02 02:44:00 +0600

    social login done

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,64 +1,72 @@
 import { Button, Label, TextInput } from "flowbite-react";
 import React from "react";
-
+import { Link } from "react-router-dom";
+import { FaGithub, FaGoogle } from "react-icons/fa";
 const Login = () => {
   return (
-    <div className="mx-auto w-1/3 mt-4 mb-10">
-      <h1 className="text-center text-2xl font-semibold my-4">
-        Please Register
-      </h1>
-      <form className="flex flex-col gap-4">
-        <div>
-          <div className="mb-2 block">
-            <Label value="Your Name" />
+    <div>
+      <div className="mx-auto w-1/3 mt-4 mb-8">
+        <h1 className="text-center text-2xl font-bold my-4">Please Login</h1>
+        <form className="flex flex-col gap-4">
+          <div>
+            <div className="mb-2 block">
+              <Label value="Your email" />
+            </div>
+            <TextInput
+              id="email2"
+              type="email"
+              placeholder="Enter Your Email"
+              required={true}
+              shadow={true}
+            />
           </div>
-          <TextInput
-            id="name"
-            type="text"
-            placeholder="Enter Your Name"
-            required={true}
-            shadow={true}
-          />
-        </div>
-        <div>
-          <div className="mb-2 block">
-            <Label value="Your email" />
+          <div>
+            <div className="mb-2 block">
+              <Label value="Your password" />
+            </div>
+            <TextInput
+              id="password2"
+              type="password"
+              placeholder="Enter Your Password"
+              required={true}
+              shadow={true}
+            />
           </div>
-          <TextInput
-            id="email2"
-            type="email"
-            placeholder="Enter Your Email"
-            required={true}
-            shadow={true}
-          />
-        </div>
-        <div>
-          <div className="mb-2 block">
-            <Label value="Your password" />
-          </div>
-          <TextInput
-            id="password2"
-            type="password"
-            placeholder="Enter Your Password"
-            required={true}
-            shadow={true}
-          />
-        </div>
-        <div>
-          <div className="mb-2 block">
-            <Label value="Photo URL" />
-          </div>
-          <TextInput
-            id="name"
-            type="text"
-            placeholder="Enter Your Photo URL"
-            required={true}
-            shadow={true}
-          />
-        </div>
 
-        <Button type="submit">Register new account</Button>
-      </form>
+          <Button type="submit">Login</Button>
+          <p className="text-secondary">
+            Don't Have an Account?{" "}
+            <span className="text-blue-600">
+              <Link to="/register">Register</Link>
+            </span>
+          </p>
+        </form>
+      </div>
+      <div className="border-blue-700 border-solid border rounded pt-2 pb-4 mt-4 mb-20 w-2/5 mx-auto">
+        <p className="text-center my-2 text-lg font-semibold text-blue-700">
+          Or Login With
+        </p>
+        <div className="d-flex justify-center">
+          <div className="flex justify-center items-center gap-2">
+            <div>
+              <Button outline={true} gradientDuoTone="redToYellow">
+                <span className="text-blue-500 flex items-center">
+                  <FaGoogle className="mr-2 h-3 w-3 "></FaGoogle>
+                  Login with Google
+                </span>
+              </Button>
+            </div>
+            <div>
+              <Button outline={true} gradientDuoTone="redToYellow">
+                <span className="text-blue-500 flex items-center">
+                  <FaGithub className="mr-2 h-3 w-3 "></FaGithub>
+                  Login with Github
+                </span>
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,9 +1,69 @@
+import { Button, Label, TextInput } from "flowbite-react";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   return (
-    <div>
-      <h2>This is register</h2>
+    <div className="mx-auto w-1/3 mt-4 mb-10">
+      <h1 className="text-center text-2xl font-bold my-4">Please Register</h1>
+      <form className="flex flex-col gap-4">
+        <div>
+          <div className="mb-2 block">
+            <Label value="Your Name" />
+          </div>
+          <TextInput
+            id="name"
+            type="text"
+            placeholder="Enter Your Name"
+            required={true}
+            shadow={true}
+          />
+        </div>
+        <div>
+          <div className="mb-2 block">
+            <Label value="Your email" />
+          </div>
+          <TextInput
+            id="email2"
+            type="email"
+            placeholder="Enter Your Email"
+            required={true}
+            shadow={true}
+          />
+        </div>
+        <div>
+          <div className="mb-2 block">
+            <Label value="Your password" />
+          </div>
+          <TextInput
+            id="password2"
+            type="password"
+            placeholder="Enter Your Password"
+            required={true}
+            shadow={true}
+          />
+        </div>
+        <div>
+          <div className="mb-2 block">
+            <Label value="Photo URL" />
+          </div>
+          <TextInput
+            id="name"
+            type="text"
+            placeholder="Enter Your Photo URL"
+            required={true}
+            shadow={true}
+          />
+        </div>
+
+        <Button type="submit">Register Now</Button>
+        <p className="text-secondary">
+          Already Have an Account?{" "}
+          <span className="text-blue-600">
+            <Link to="/login">Login</Link>
+          </span>
+        </p>
+      </form>
     </div>
   );
 };
